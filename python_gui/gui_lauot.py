12cc1ef413578898d521b79b48c16296e3d8379a
Hunger <2005.maksim.andreev@gmail.com>
2025-09-30 12:26:52 +0300

    reorg

--- python_gui/gui_lauot.py
+++
@@ -1,1 +0,0 @@
-# pip install -r requirements.txt потом скачать в терминале библиотеки
---
+++ ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+ui <- fluidPage(
+
+  titlePanel("Greetings"), # изменить после отладки
+
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Import CSV file"),
+      selectInput("method", "Method of clusterization",
+                  choices = c("K-means", "DBSCAN", "hierarchical")),
+      numericInput("k", "Number of clusters", value =  3, min = 2, max = 10),
+      sliderInput("eps",
+                  "distance threshold (eps)",
+                  min = 0.1,
+                  max = 5,
+                  value = 0.5,
+                  step = 0.1),
+      actionButton("run", "Run Analysys")
+    ),
+    mainPanel(
+      plotOutput("cluster_Plot"),
+      tableOutput("summury"),
+      textOutput("info")
+    )
+  )
+)
